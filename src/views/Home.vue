87b3c77ed14c487f2bdcb0231694156638740393
Andrea Stagi <stagi.andrea@gmail.com>
2021-01-04 23:52:27 +0100

    Fix claim

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
     <p
       class="max-w-screen-lg text-lg sm:text-2xl sm:leading-10 font-medium mb-10 sm:mb-11 text-main dark:text-main-dark"
     >
-      <b class="text-primary dark:text-primary-dark">OrugaUI + TailwindCSS 2 demo</b> shows how to customize <b>Oruga UI</b> with <b>TailwindCSS</b> and use components like Input, Radio, Loading, Switch Collapse and so on!
+      <b class="text-primary dark:text-primary-dark">OrugaUI + TailwindCSS 2 demo</b> shows how to customize <b>Oruga UI</b> with <b>TailwindCSS</b> and use components like Input, Radio, Loading, Switch, Collapse and so on!
     </p>
     <h1>Recipes</h1>
     <recipes-finder />
