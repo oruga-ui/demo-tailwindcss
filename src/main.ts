0cefe8493506514711792718639d641fcb2ae374
Andrea Stagi <stagi.andrea@gmail.com>
2021-01-24 01:27:51 +0100

    Fix Healthy Beef Burger image url

--- src/main.ts
+++ src/main.ts
@@ -69,7 +69,7 @@ const store = new Vuex.Store({
         difficulty: "easy",
         servings: 4,
         procedure: "Lily’s Healthy Beef Burger recipe, sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
-        image: "https://tailwindcss.com/_next/static/media/healthy-beef-burger.e735f49f7da4c92099f8811abf828ea8.jpg"
+        image: "https://images-na.ssl-images-amazon.com/images/I/81cMw0qNjTL._SX425_.jpg"
       }
     ],
   },
